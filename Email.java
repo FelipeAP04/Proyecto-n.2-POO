4c0e9bfc77ee59009ee67417bbbfaeff0cfc9a4d
FelipeAP04 <agu23195@uvg.edu.gt>
2023-10-06 19:37:23 -0600

    Documentacion

--- Email.java
+++ Email.java
@@ -1,8 +1,9 @@
-package emailapp;
+package emailapp;//meter archivo en el paquete
 
-import java.util.Scanner;
+import java.util.Scanner;//importar Java Scanner
 
-public class Email {
+public class Email { //definir clase
+    //crear variables de la clase
     private String firstName;
 	private String lastName;
 	private String password;
@@ -13,15 +14,17 @@ public class Email {
 	private String alternateEmail;
 	private String companySuffix = "UNI.com";
 
-    public Email(String firstName, String lastName){
-		this.firstName = firstName;
+    public Email(String firstName, String lastName){ //Constructor de la clase
+		//llama firstName, lastName y department en el constructor
+        this.firstName = firstName;
 		this.lastName = lastName;
+		this.department = setDepartment(); //pregunta por el departamento del usuario
 		
-		this.department = setDepartment();
-		
+        //define una contraseña
 		this.password = randomPasssword(defaultPasswordLenght);
 		System.out.println("Tú contraseña es: " + this.password);
 
+        //cra email personalizado
         email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
 		System.out.println("Tu correo es: " + email);
     }
