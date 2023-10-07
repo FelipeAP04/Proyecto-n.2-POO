86add42dd21a022a574af21ea3371e785f86673c
FelipeAP04 <agu23195@uvg.edu.gt>
2023-10-06 19:15:51 -0600

    Se guardan los datos de los scanners

--- EmailApp.java
+++ EmailApp.java
@@ -6,11 +6,16 @@ public class EmailApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
         System.out.print("Ingrese su primer nombre: ");
+        String firstName = scanner.nextLine();
         
         System.out.print("Ingrese su apellido: ");
-
+        String lastName = scanner.nextLine();
+        
+        Email em1 = new Email(firstName, lastName);
+        System.out.println(em1.showInfo());
+        
         scanner.close();
     }
 }
