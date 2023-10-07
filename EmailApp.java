c38ed76dad215b1ffc10b5b83b786575abf85d7c
FelipeAP04 <agu23195@uvg.edu.gt>
2023-10-06 19:13:46 -0600

    Se crean scaners para usar posteriormente en la clase

--- EmailApp.java
+++ EmailApp.java
@@ -6,6 +6,11 @@ public class EmailApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    }
 
+        System.out.print("Ingrese su primer nombre: ");
+        
+        System.out.print("Ingrese su apellido: ");
+
+        scanner.close();
+    }
 }
