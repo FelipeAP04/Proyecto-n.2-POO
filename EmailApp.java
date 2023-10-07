b878a0df33f91c371837edb10fee64a694d14f68
FelipeAP04 <agu23195@uvg.edu.gt>
2023-10-06 19:12:23 -0600

    Se crea la base y se importa scanner de Java

---
+++ EmailApp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+}
