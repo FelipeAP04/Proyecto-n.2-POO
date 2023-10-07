44547ed1e111692fd6a9e50f9f959a5a7112b2e2
FelipeAP04 <agu23195@uvg.edu.gt>
2023-10-06 19:22:50 -0600

    Se crean variables de la clase

--- Email.java
+++ Email.java
@@ -3,5 +3,15 @@ package emailapp;
 import java.util.Scanner;
 
 public class Email {
+    private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String email;
+	private int mailboxCapacity = 500;
+	private int defaultPasswordLenght = 10;
+	private String alternateEmail;
+	private String companySuffix = "UNI.com";
+
     
 }
