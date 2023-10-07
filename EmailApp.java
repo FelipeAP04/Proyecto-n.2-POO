06a29f3772dded00e6a6de400c82d9b5414aaf89
FelipeAP04 <agu23195@uvg.edu.gt>
2023-10-06 19:18:50 -0600

    Documentacion de EmailApp

--- EmailApp.java
+++ EmailApp.java
@@ -1,21 +1,21 @@
-package emailapp;
+package emailapp; //meter archivo en el paquete
 
-import java.util.Scanner;
+import java.util.Scanner; //importar Java Scanner
 
 public class EmailApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //crear nuevo scaner
         
         System.out.print("Ingrese su primer nombre: ");
-        String firstName = scanner.nextLine();
+        String firstName = scanner.nextLine(); //pedir nombre de usuario
         
         System.out.print("Ingrese su apellido: ");
-        String lastName = scanner.nextLine();
+        String lastName = scanner.nextLine(); // pedir apellido de usuario
         
         Email em1 = new Email(firstName, lastName);
-        System.out.println(em1.showInfo());
+        System.out.println(em1.showInfo()); //crear correo con datos inscritos
         
-        scanner.close();
+        scanner.close(); // cerrar scaner cuando se termina de usar
     }
 }
