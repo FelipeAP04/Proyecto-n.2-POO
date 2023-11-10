b50dee9f465931d5b6ff8f520386667453ada382
Hb_W <hugobarillasvi@hotmail.com>
2023-11-09 23:41:23 -0600

    Gui eliminar componente

--- Emailapp/src/main/java/Emailapp/Correos.java
+++ Emailapp/src/main/java/Emailapp/Correos.java
@@ -21,10 +21,6 @@ public class Correos extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaemails = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        estudianteRb = new javax.swing.JRadioButton();
-        profesorRb = new javax.swing.JRadioButton();
-        otroRb = new javax.swing.JRadioButton();
         mostrarBt = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -45,22 +41,6 @@ public class Correos extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 29)); // NOI18N
         jLabel1.setText("Visualizar correos");
-
-        jLabel2.setText("¿A qué departamento pertenece?");
-
-        Departamento.add(estudianteRb);
-        estudianteRb.setText("Estudiante");
-
-        Departamento.add(profesorRb);
-        profesorRb.setText("Profesor");
-        profesorRb.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                profesorRbActionPerformed(evt);
-            }
-        });
-
-        Departamento.add(otroRb);
-        otroRb.setText("Otro");
 
         mostrarBt.setBackground(new java.awt.Color(204, 204, 204));
         mostrarBt.setText("Mostrar");
@@ -83,15 +63,6 @@ public class Correos extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(8, 8, 8)
                                 .addComponent(mostrarBt, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(65, 65, 65)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(6, 6, 6)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(profesorRb)
-                                    .addComponent(estudianteRb)
-                                    .addComponent(otroRb))))
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 620, Short.MAX_VALUE))
                 .addContainerGap())
@@ -100,20 +71,9 @@ public class Correos extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 23, Short.MAX_VALUE)
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(estudianteRb)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(profesorRb)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(otroRb))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(18, 18, 18)
-                        .addComponent(mostrarBt, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(mostrarBt, javax.swing.GroupLayout.DEFAULT_SIZE, 67, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -142,10 +102,6 @@ public class Correos extends javax.swing.JFrame {
         // Puedes manejar la excepción de alguna manera, por ejemplo, mostrando un mensaje de error
     }
     }//GEN-LAST:event_mostrarBtActionPerformed
-
-    private void profesorRbActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_profesorRbActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_profesorRbActionPerformed
 
 
     public static void main(String args[]) {
@@ -182,13 +138,9 @@ public class Correos extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup Departamento;
-    private javax.swing.JRadioButton estudianteRb;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton mostrarBt;
-    private javax.swing.JRadioButton otroRb;
-    private javax.swing.JRadioButton profesorRb;
     private javax.swing.JTable tablaemails;
     // End of variables declaration//GEN-END:variables
 }
